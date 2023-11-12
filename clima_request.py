5044729a86e6bb598689fcff57ac90db1746e489
Marco Vomiero <colocomarco1@gmail.com>
2023-11-12 18:51:52 +0100

    API set and works

---
+++ clima_request.py
@@ -0,0 +1,31 @@
+from config import API_KEY
+
+import requests
+
+print(API_KEY)
+
+
+# Replace these coordinates with the latitude and longitude of your plant location
+latitude = 52.52  # Example latitude (Berlin)
+longitude = 13.4  # Example longitude (Berlin)
+
+# Replace with the desired month (1 for January, 2 for February, etc.)
+month = 2
+
+# OpenWeatherMap Aggregated Monthly Historical Weather API endpoint
+endpoint = 'https://history.openweathermap.org/data/2.5/aggregated/month'
+
+# Construct the API URL
+url = f'{endpoint}?month={month}&lat={latitude}&lon={longitude}&appid={API_KEY}'
+
+# Make the API request
+response = requests.get(url)
+
+# Check if the request was successful (status code 200)
+if response.status_code == 200:
+    # Parse and work with the response data (which is typically in JSON format)
+    weather_data = response.json()
+    print(weather_data)
+else:
+    # Print an error message if the request was not successful
+    print(f'Error: {response.status_code}, {response.text}')
